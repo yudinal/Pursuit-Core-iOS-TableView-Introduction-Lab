55b46bf519aeac8961f00bc3ce81251047faf5cf
Lilia Yudina <liliayudina@gmail.com>
2019-11-19 16:45:35 -0500

    completed the subtitle/date part

---
+++ TableViewIntroductionLab/TableViewIntroductionLab/DetailViewController.swift
@@ -0,0 +1,19 @@
+//
+//  DetailViewController.swift
+//  TableViewIntroductionLab
+//
+//  Created by Lilia Yudina on 11/15/19.
+//  Copyright © 2019 Benjamin Stone. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
--- TableViewIntroductionLab/TableViewIntroductionLab/ViewController.swift
+++ TableViewIntroductionLab/TableViewIntroductionLab/ViewController.swift
@@ -8,13 +8,58 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
 
+    
+
+    @IBOutlet weak var taskList: UITableView!
+    
+    private var tasks = Task.allTasks {
+        didSet {
+            taskList.reloadData()
+        }
+    }
+    
+    private var sortAscending = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        taskList.dataSource = self
+        sortTasks(true)
+    }
+    lazy var dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "EEEE, MMM d, yyyy h:mm a"
+      return formatter
+    }()
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = taskList.dequeueReusableCell(withIdentifier: "SectionCell", for: indexPath)
+        let task = tasks[indexPath.row]
+        cell.textLabel!.text = task.name
+        cell.detailTextLabel!.text = dateFormatter.string(from: task.dueDate)
+    
+        return cell
     }
 
-
+    @IBAction func UIButtonPressed(_ sender: UIBarButtonItem) {
+       sortAscending.toggle()
+        print()
+        sortTasks(sortAscending)
+        print("here")
+    }
+    
+    func sortTasks(_ sortAscending: Bool) {
+        if sortAscending {
+            tasks = Task.allTasks.sorted { $0.name < $1.name }
+            navigationItem.rightBarButtonItem?.title = "Sort Descending"
+        } else {
+            tasks = Task.allTasks.sorted { $0.name > $1.name }
+            navigationItem.rightBarButtonItem?.title = "Sort Ascending"
+        }
+    }
 }
 
